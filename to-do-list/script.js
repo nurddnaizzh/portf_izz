c26eeaa954a8f2471c2fce00c1588f8356041ef4
nurddnaizzh <nuraddinaizzah210602@gmail.com>
2026-01-31 18:26:07 +0800

    saveData() function into storage (updated tasks) and showTask() function to display the li

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -17,13 +17,24 @@ function addTask() {
     }
     // Clear the input box
     inputBox.value = "";
+    saveData();
 }
 
 listContainer.addEventListener("click", function(e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
+        saveData();
     }
     else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+        saveData();
     }
 }, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
