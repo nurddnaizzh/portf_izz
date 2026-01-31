4b91be567c2408fc81b3bc6c1e8a61976481b251
nurddnaizzh <nuraddinaizzah210602@gmail.com>
2026-01-31 18:20:37 +0800

    enable checked function and remove tasks function

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -18,3 +18,12 @@ function addTask() {
     // Clear the input box
     inputBox.value = "";
 }
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+}, false);
