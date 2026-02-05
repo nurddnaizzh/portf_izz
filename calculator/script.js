40f528404a4f6e064507cc4236a2127cad362c2a
nurddnaizzh <nuraddinaizzah210602@gmail.com>
2026-02-05 22:33:52 +0800

    finished!! thanks to eval() function that convert the operators in the string

--- calculator/script.js
+++ calculator/script.js
@@ -10,9 +10,18 @@ arr.forEach(button => {
             string = eval(string);
             input.value = string;
         }
-
+        else if (e.target.innerHTML == 'AC') {
+            string = "";
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+        else {
         // make the numbers stack when clicked
         string += e.target.innerHTML; // add the clicked button's value to the string
         input.value = string; // display the updated string in the input box
+        }
     })
 })
