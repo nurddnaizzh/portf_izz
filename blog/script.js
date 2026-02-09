acc51505c3eedf719624bef26d3c6e543225f7c2
nurddnaizzh <nuraddinaizzah210602@gmail.com>
2026-02-09 23:35:47 +0800

    add css and script and chevron arrows to post slider

--- blog/script.js
+++ blog/script.js
@@ -1,7 +1,18 @@
+// open menu toggle for mobile/tablet screen
 $(document).ready(function() {
     $('.menu-toggle').on('click', function() {
         // show height 100em for nav
         $('.nav').toggleClass('showing');
         $('.nav ul').toggleClass('showing');
+    });
+
+    // Slick carousel initialization
+    $('.post-wrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
     });
 });
