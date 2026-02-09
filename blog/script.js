aa6fb48c164f698d04bda1ffae9e3e766f4e9966
nurddnaizzh <nuraddinaizzah210602@gmail.com>
2026-02-10 00:34:24 +0800

    decorate post info including add image, infos, and responsive

--- blog/script.js
+++ blog/script.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
         $('.nav ul').toggleClass('showing');
     });
 
+    // slick link: https://kenwheeler.github.io/slick/
+
     // Slick carousel initialization
     $('.post-wrapper').slick({
         slidesToShow: 3,
@@ -14,5 +16,33 @@ $(document).ready(function() {
         autoplaySpeed: 2000,
         nextArrow: $('.next'),
         prevArrow: $('.prev'),
+        responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
     });
 });
