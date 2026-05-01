87aface50e694ee21e2e9ff43d2aab458dfc8c63
nurddnaizzh <nuraddinaizzah210602@gmail.com>
2026-05-01 23:03:30 +0800

    start and finish the countdown timer project for html, css and js

---
+++ public/countdown timer/script.js
@@ -0,0 +1,23 @@
+const Days = document.getElementById("days");
+const Hours = document.getElementById("hours");
+const Minutes = document.getElementById("minutes");
+const Seconds = document.getElementById("seconds");
+
+const targetDate = new Date("June 21 2026 00:00:00").getTime();
+
+function timer() {
+    const currentDate = new Date().getTime();
+    const distance = targetDate - currentDate;
+
+    const days = Math.floor(distance / 1000 / 60 / 60 / 24);
+    const hours = Math.floor(distance / 1000 / 60 / 60) % 24;
+    const minutes = Math.floor(distance / 1000 / 60) % 60;
+    const seconds = Math.floor(distance / 1000) % 60;
+
+    Days.innerHTML = days;
+    Hours.innerHTML = hours;
+    Minutes.innerHTML = minutes;
+    Seconds.innerHTML = seconds;
+}
+
+setInterval(timer, 1000);
