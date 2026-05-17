418f7eb8e11073f8a7c822304ccc977bc8d3b758
nurddnaizzh <nuraddinaizzah210602@gmail.com>
2026-05-17 23:41:10 +0800

    Creating Options Tag & Inserting it inside Select Tag

--- public/currency converter/script.js
+++ public/currency converter/script.js
@@ -1,9 +1,23 @@
 const dropList = document.querySelectorAll(".drop-list select");
+getButton = document.querySelector(".form button");
 
 for (let i = 0; i < dropList.length; i++) {
     for (currency_code in country_code) {
+        // selecting USD by efault as FROM currency and MYR as TO currency
+        let selected;
+        if (i == 0) {
+            selected = currency_code == "USD" ? "selected" : "";
+        } else if (i == 1) {
+            selected = currency_code == "MYR" ? "selected" : "";
+        }
+
         // creating option tag with currency code as value and text
         let optionTag = `<option value="${currency_code}">${currency_code}</option>`;
         dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }
 }
+
+getButton.addEventListener("click", e => {
+    e.preventDefault(); // preventing form from submitting
+    getExchangeRate();
+});
