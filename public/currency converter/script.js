554ff4ffdd70f53dbf8a796aa7ec16ab3d648565
nurddnaizzh <nuraddinaizzah210602@gmail.com>
2026-05-19 00:11:32 +0800

    apply console to see totalExchangeRate, and change some code errors about the selected in optionTag (tertinggal selected, and no wonder la both used the same AED rate and not respectives country's currency)

--- public/currency converter/script.js
+++ public/currency converter/script.js
@@ -1,18 +1,20 @@
 const dropList = document.querySelectorAll(".drop-list select");
-getButton = document.querySelector(".form button");
+fromCurrency = document.querySelector(".from select");
+toCurrency = document.querySelector(".to select");
+getButton = document.querySelector("form button");
 
 for (let i = 0; i < dropList.length; i++) {
     for (currency_code in country_code) {
         // selecting USD by efault as FROM currency and MYR as TO currency
         let selected;
         if (i == 0) {
-            selected = currency_code == "USD" ? "selected" : "";
-        } else if (i == 1) {
             selected = currency_code == "MYR" ? "selected" : "";
+        } else if (i == 1) {
+            selected = currency_code == "USD" ? "selected" : "";
         }
 
         // creating option tag with currency code as value and text
-        let optionTag = `<option value="${currency_code}">${currency_code}</option>`;
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
         dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }
 }
@@ -32,5 +34,12 @@ function getExchangeRate() {
         amountVal = 1;
     }
 
-    let url = 'https://v6.exchangerate-api.com/v6/b66028e64b6bfd1e9c13183e/latest/USD';
+    let url = `https://v6.exchangerate-api.com/v6/b66028e64b6bfd1e9c13183e/latest/${fromCurrency.value}`;
+
+    // fetching api response and returning it with parsing into js object and in another then method receiving that object
+    fetch(url).then(response => response.json()).then(result => {
+        let exchangeRate = result.conversion_rates[toCurrency.value];
+        let totalExchangeRate = (amountVal * exchangeRate).toFixed(2);
+        console.log(totalExchangeRate);
+    });
 }
