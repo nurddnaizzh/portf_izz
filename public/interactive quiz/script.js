597b5de09da51e090f3fad10ee8d37ccb97d38c2
nurddnaizzh <nuraddinaizzah210602@gmail.com>
2026-06-04 20:31:41 +0800

    add function that stores questions as well as correct answers among the provided choices

---
+++ public/interactive quiz/script.js
@@ -0,0 +1,38 @@
+const question = [
+    {
+        questions: "Which is the largest animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        questions: "Which is the smallest country in the world?",
+        answers: [
+            {text: "Vatican City", correct: true},
+            {text: "Bhutan", correct: false},
+            {text: "Nepal", correct: false},
+            {text: "Sri Lanka", correct: false},
+        ]
+    },
+    {
+        questions: "Which is the largest desert in the world?",
+        answers: [
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: false},
+            {text: "Antarctica", correct: true},
+        ]
+    },
+    {
+        questions: "Which is the smallest continent in the world?",
+        answers: [
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false},
+        ]
+    }
+];
